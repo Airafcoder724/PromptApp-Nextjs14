9a8662725664e3c81e415b5627832a4eb62f75c3
Airafcoder724 <airaf724@gmail.com>
2024-02-05 18:36:25 +0530

    alt check

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -21,7 +21,7 @@ const Nav = () => {
    <nav className='flex-between w-full mb-16 pt-3'>
 
     <Link href="/" className='flex gap-2 flex-center'>
-        <Image className='object-contain' src='/assets/images/logo.svg' width={40} height={40} />
+        <Image className='object-contain' alt='logo' src='/assets/images/logo.svg' width={40} height={40} />
         <p className='logo_text'>Promtopia</p>
     </Link>
 
